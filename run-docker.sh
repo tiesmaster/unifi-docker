a6126cda9157824338ffdedce350ede3f145d40b
Thijs Brobbel <thijsbrobbel@gmail.com>
2022-05-31 00:19:21 +0200

    Update run command

--- run-docker.sh
+++ run-docker.sh
@@ -3,4 +3,4 @@
 # mkdir -p unifi/data
 # mkdir -p unifi/log
 # docker run --rm --init -p 8080:8080 -p 8443:8443 -p 3478:3478/udp -e TZ='Europe/Amsterdam' -v ~/unifi:/unifi --name unifi jacobalberty/unifi:v7
-docker run --rm --init -p 8080:8080 -p 8443:8443 -p 3478:3478/udp -e TZ='Europe/Amsterdam' --name unifi jacobalberty/unifi
+docker run --rm -it --init -p 8080:8080 -p 8443:8443 -p 3478:3478/udp -e TZ='Europe/Amsterdam' -v unifi:/unifi --name unifi jacobalberty/unifi
