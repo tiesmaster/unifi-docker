99a25e5664c4825695475463ff5309d6c9e5f88c
Thijs Brobbel <thijsbrobbel@gmail.com>
2022-06-27 16:42:21 +0200

    Consolidate logic to single place

--- upload-unifi-backups.sh
+++ upload-unifi-backups.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo Copying files out of the unifi docker container into /var/local/unifi_backups
+docker cp -a unifi:/unifi/data/backup/autobackup /var/local/unifi_backups
+
 cd /var/local/unifi_backups/autobackup
 
 for file in *
