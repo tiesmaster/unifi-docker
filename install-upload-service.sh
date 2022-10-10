ffc49b257c77c8bbd5af31330ee1253482906b9b
Thijs Brobbel <thijsbrobbel@gmail.com>
2022-10-10 22:42:19 +0200

    Add sudo everywhere in the install upload script

--- install-upload-service.sh
+++ install-upload-service.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-[ -d /var/local/unifi_backups ] || mkdir /var/local/unifi_backups
-chown ties /var/local/unifi_backups
+[ -d /var/local/unifi_backups ] || sudo mkdir /var/local/unifi_backups
+sudo chown ties /var/local/unifi_backups
 
-cp upload-unifi-backups.sh /usr/local/bin/
+sudo cp upload-unifi-backups.sh /usr/local/bin/
 
-cp upload-unifi-controller-backups.service /etc/systemd/system/
-cp upload-unifi-controller-backups.timer /etc/systemd/system/
+sudo cp upload-unifi-controller-backups.service /etc/systemd/system/
+sudo cp upload-unifi-controller-backups.timer /etc/systemd/system/
 
-systemctl enable upload-unifi-controller-backups.timer
-systemctl start upload-unifi-controller-backups.timer
+sudo systemctl enable upload-unifi-controller-backups.timer
+sudo systemctl start upload-unifi-controller-backups.timer
 
-systemctl daemon-reload
+sudo systemctl daemon-reload
