4ba5de1ca9a6cc61dfe010cd67a373cabf43a357
Thijs Brobbel <thijsbrobbel@gmail.com>
2022-10-10 22:29:59 +0200

    Update upload script to backup to .../wasp-backup/

--- upload-unifi-backups.sh
+++ upload-unifi-backups.sh
@@ -8,5 +8,5 @@ cd /var/local/unifi_backups/autobackup
 for file in *
 do
     echo Uploading file ${file}
-    dbxcli put ${file} unifi_backups/${file}
+    dbxcli put ${file} unifi_backups/wasp-backup/${file}
 done
